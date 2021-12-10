307c2f9f3fe2bf5a9159d1e1e8e82be9cccbef94
126Florian <91480650+126Florian@users.noreply.github.com>
2021-12-11 00:47:41 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,4 +1,4 @@
 #ifndef ESERCIZIO8_LIB_H
 #define ESERCIZIO8_LIB_H
-float numeroMassimo(const float numeri [], int dimensione);
+float numeroMassimo(const float numeri[], int dimensione);
 #endif //ESERCIZIO8_LIB_H
